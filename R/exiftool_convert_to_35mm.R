f984a229a2ef115f89df3af30ae5bf2b6bc7a230
insilentio <insilentio@protonmail.ch>
2025-06-24 13:54:09 +0200

    roxygenise, return handling

--- R/exiftool_convert_to_35mm.R
+++ R/exiftool_convert_to_35mm.R
@@ -1,28 +1,36 @@
-# amend crop factor information
-# (hardcoded in a mapping table)
-# mapping table
-
-
-convert35 <- function(path, csv_execute = TRUE){
-  require("exiftoolr")
+#' amend crop factor information for focallength bases on a mapping table
+#' 
+#' @description Writes the tag for equivalent focal length in 35mm format into photo metadata.
+#' Relies on a hardcoded mapping table (CropFactor.csv).
+#' It uses per default the csv option of exifool to handle different values for different files,
+#' which is much faster than a for loop which calls exiftool every time.
+#' Exiftool per default creates copies of the original files (*_original); this behaviour can be modified by parameter.
+#'
+#' @param paths List of photos to be modified. Needs a character vector with full file names
+#' @param csv_execute logical, to determine whether modification should be directly written or returned as tibble
+#' @param csv_path path and file name of the output csv file
+#' @param delete_original whether the original copies of the photos should be deleted afterwards or not
+#'
+#' @returns depending on param csv_execute:
+#' - if TRUE,  writes the tag values as csv file and writes them via exiftool to pictures
+#' - if FALSE, returns the tags as tibble
+#' @export
+#'
+#' @examples convert35("Test.jpg")
+convert35 <- function(paths,
+                      csv_execute = TRUE,
+                      csv_path = '~/Pictures/locations.csv',
+                      delete_original = FALSE){
+  
+  require(exiftoolr)
+  require(dplyr)
   
   mapping <- read_csv("Data/CropFactor.csv")
   
   args <- c("-G", "-s", "-n", "-exif:focallength", "-exif:model")
-  focallength <- exif_read(args = args, path = path)
-  
-  focallength <- focallength |> 
+  focallength <- exif_read(args = args, path = path) |> 
     left_join(mapping, by = join_by("EXIF:Model" == "model")) |> 
     mutate(`EXIF:FocalLengthIn35mmFormat` = `EXIF:FocalLength`*factor)
   
-  if (csv_execute) {
-    # now use the csv option of exifool to handle different values for different files
-    # this is much faster than a for loop which calls exiftool every time
-    loc_path <- normalizePath('~/Pictures/fl.csv', mustWork = FALSE)
-    focallength |> write_csv(loc_path)
-
-  exif_call(args = c("-f", paste0("-csv=", loc_path)), path = path)
-  } else {
-    focallength
-  }
+  handle_return(focallength, csv_execute, paths, csv_path, delete_original)
 }
