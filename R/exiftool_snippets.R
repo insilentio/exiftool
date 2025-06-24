57580e95cab7e27aa5fb0439dfaef5452f4a6821
insilentio <insilentio@protonmail.ch>
2025-06-24 10:27:21 +0200

    added function handle_return()

--- R/exiftool_snippets.R
+++ R/exiftool_snippets.R
@@ -1,4 +1,3 @@
-
 #' Read PhotoStatistica exif_export.csv for path creation
 #' 
 #' The function extracts the different info from the exif_export file from PhotoStatistica
@@ -12,13 +11,12 @@
 #'
 #' @examples extract_paths()
 extract_paths <- function(input = "~/Pictures/Exif-export.csv") {
-  paths <- read_csv(input, trim_ws = FALSE) |> 
+  read_csv(input, trim_ws = FALSE) |> 
     select(1, 5, "Objektivmodell") |> 
     rename(file = 1, path = 2, type = 3) |> 
     mutate(full = paste0(path, "/", file))
-  
-  paths
 }
+
 
 #' Write complete path names into csv file
 #'
@@ -31,8 +29,39 @@ extract_paths <- function(input = "~/Pictures/Exif-export.csv") {
 #' @examples output_paths(extract_paths())
 output_paths <- function(paths,
                          output = "~/Pictures/paths.txt") {
-  write_csv2(paths |> 
-             select(full),
+  write_csv2(paths |> select(full),
              output,
              col_names = FALSE)
+}
+
+
+#' Handles the outcome of the various exiftool functions
+#' 
+#' @description depending on param csv_execute, either writes a csv_file
+#'
+#' @param df tibble or dataframe with the file and metadata information. Usually created within one
+#' of the exiftool_ file functions.
+#' @param csv_execute logical, to determine whether modification should be directly written or returned as tibble
+#' @param paths List of photos to be modified. Needs a character vector with full file names
+#' @param csv_path path and file name of the output csv file
+#' @param delete_original whether the original copies of the photos should be deleted afterwards or not
+#'
+#' @returns depending on param csv_execute:
+#' - if TRUE,  writes the tag values as csv file and writes them via exiftool to pictures
+#' - if FALSE, returns the tags as tibble
+#' @export
+#'
+#' @examples
+handle_return <- function(df, csv_execute, paths, csv_path, delete_original) {
+  if (csv_execute){
+    loc_path <- normalizePath(csv_path, mustWork = FALSE)
+    df |> write_csv(loc_path)
+    
+    exif_call(args = c("-f", paste0("-csv=", loc_path)), path = paths)
+    
+    if (delete_original)
+      exif_call(args = c("-r", "-delete_original!"), path = unique(dirname(paths)))
+  } else {
+    df
+  }
 }
